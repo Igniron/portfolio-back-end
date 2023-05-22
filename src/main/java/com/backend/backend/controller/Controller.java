a6fce3851257828b3e152c888026d53d822b17b5
Nacho <roncalnacho98@gmail.com>
2023-05-22 09:47:01 -0300

    Request update

--- src/main/java/com/backend/backend/controller/Controller.java
+++ src/main/java/com/backend/backend/controller/Controller.java
@@ -101,55 +101,26 @@ public class Controller {
     }
     
     @PostMapping("/update/person")
-    public void updatePerson(@RequestParam Long id, @RequestParam String name, @RequestParam String profession, @RequestParam String description, @RequestParam String img)
+    public void updatePerson(@RequestBody Person per)
     {
-        Person per = new Person();
-        per.setId(id);
-        per.setName(name);
-        per.setProfession(profession);
-        per.setDescription(description);
-        per.setImg(img);
         persServ.updatePerson(per);
     }
     
      @PostMapping("/update/education")
-    public void updateEducation(@RequestParam Long id,@RequestParam String name, @RequestParam String title, @RequestParam String state, @RequestParam String average, @RequestParam String link)
+    public void updateEducation(@RequestBody Education edu)
     {
-        Education edu = new Education();
-        edu.setId(id);
-        edu.setName(name);
-        edu.setTitle(title);
-        edu.setState(state);
-        edu.setAverage(average);
-        edu.setLink(link);
         eduServ.updateEducation(edu);
     }
     
      @PostMapping("/update/project")
-    public void updateProject(@RequestParam Long id, @RequestParam String title, @RequestParam String description, @RequestParam String link, @RequestParam String img)
+    public void updateProject(@RequestBody Project pro)
     {
-        Project pro = new Project();
-        pro.setId(id);
-        pro.setTitle(title);
-        pro.setDescription(description);
-        pro.setLink(link);
-        pro.setImg(img);
         proServ.updateProject(pro);
     }
     
      @PostMapping("/update/contact")
-    public void updateContact(@RequestParam Long id, @RequestParam String home, @RequestParam String location, @RequestParam String phone, @RequestParam String email, @RequestParam String instagram, @RequestParam String twitter, @RequestParam String linkedin, @RequestParam String github)
+    public void updateContact(@RequestBody Contact con)
     {
-        Contact con = new Contact();
-        con.setId(id);
-        con.setHome(home);
-        con.setLocation(location);
-        con.setPhone(phone);
-        con.setEmail(email);
-        con.setInstagram(instagram);
-        con.setTwitter(twitter);
-        con.setLinkedin(linkedin);
-        con.setGithub(github);
         contServ.updateContact(con);
     }
   
